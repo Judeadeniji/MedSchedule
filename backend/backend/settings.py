bdf6e3b2f50a37ce261d8b4d2761ce2f886d8526
Hadiz <adefemihadizi2020@gmail.com>
2024-06-13 18:04:15 +0100

    organized project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,9 +56,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'api',
+    
+    
     'rest_framework',
-    'corsheader'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
--- backend/useraccount/apps.py
+++ backend/useraccount/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class UseraccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    name = 'useraccount'
