78e73ad0b1b12dbd7db018b5826e6c4f12fa327e
judeadeniji <judeadeniji66@gmail.com>
2024-06-15 22:48:08 +0100

    added solutin section

--- src/components/home-screen.tsx
+++ src/components/home-screen.tsx
@@ -1,1 +1,15 @@
-function Solutions() {}
+function Solutions() {
+  return (
+    <div className="grid md:grid-cols-5">
+      <div className="md:col-span-2">
+        {/* image goes here */}
+      </div>
+
+      <div className="col-span-3">
+        <h1>
+          Scheduler App for Patients
+        </h1>
+      </div>
+    </div>
+  )
+}
